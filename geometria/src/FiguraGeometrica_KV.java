58d3f878e64a010b74b05fc3f78fccde45bf5873
Konstantin Voziyan <kvdprin88@gmail.com>
2019-01-25 12:24:03 +0100

    Versión refactorizada

--- geometria/src/FiguraGeometrica_KV.java
+++ geometria/src/FiguraGeometrica_KV.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_KV {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_KV(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
